e0c8cf66ec7268666cd214a729adb240c83ecb3f
Nicolas NGUYEN <nngvk@studec.fr>
2022-02-09 14:08:03 +0100

    le cruuud

---
+++ CRUD-FOR-LUC/app/Http/Controllers/ActivitiesController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Activities;
+use Illuminate\Http\Request;
+
+class ActivitiesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Activities::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $activities = Activities::create(request()->all());
+        return $activities;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Activities  $activities
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Activities $activities)
+    {
+        return Activities::all();
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Activities  $activities
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Activities $activities)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Activities  $activities
+     * @return \Illuminate\Http\Response
+     */
+    public function update($id, Request $request)
+    {
+        $activities = Activities::FindorFail($id);
+        $activities->fill($request->only(['author', 'description', 'duration', 'name','nbStars','price']));
+        $activities->save();
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Activities  $activities
+     * @return \Illuminate\Http\Response
+     */
+    public function delete($id)
+    {
+        $activities = Activities::find($id);
+        $activities -> delete(); 
+    }
+}
---
+++ CRUD-FOR-LUC/database/migrations/2022_02_09_101042_create_activities_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateActivitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('activities', function (Blueprint $table) {
+            $table->id();
+            $table->string('author');
+            $table->string('description');
+            $table->integer('duration');
+            $table->string('name');
+            $table->integer('nbStars');
+            $table->integer('price');
+            $table->timestamps();
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('activities');
+    }
+}
